2387a5e8150bb6233a1e6f4bda702ab6cbdbeb9e
Saubhagya Singh <76896543+SaubhagyaSingh@users.noreply.github.com>
2023-12-30 20:05:40 +0530

    fixed

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -8,13 +8,16 @@ const Admin = () => {
   const handleSportSelection = async (selectedSport) => {
     try {
       // Send a POST request with the selected sport name
-      const response = await fetch("https://kvdwjdqr-4000.inc1.devtunnels.ms/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title: selectedSport }),
-      });
+      const response = await fetch(
+        "https://kvdwjdqr-4000.inc1.devtunnels.ms/summit/sport-details",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title: selectedSport }),
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
