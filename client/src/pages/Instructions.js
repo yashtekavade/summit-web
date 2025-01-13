c4a2374a9956463f3f7e16c41025bfc05750b62b
Devyani Sisodia <devu060704@gmail.com>
2025-01-13 22:09:54 +0530

    testimonials

--- client/src/pages/Instructions.js
+++ client/src/pages/Instructions.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../Components/Footer/Footer";
-import i1 from "../assets/images/inst4.png";
+import i1 from "../assets/images/inst1.png";
 import "./Instructions.css";
 
 const Instructions = () => {
